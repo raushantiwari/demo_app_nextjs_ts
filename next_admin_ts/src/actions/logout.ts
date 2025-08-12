fe76546b520f7db10a33f224fe499869f379952a
Raushan <raushan.tiwari@tothenew.com>
2025-08-12 12:21:17 +0530

    code update

--- next_admin_ts/src/actions/logout.ts
+++ next_admin_ts/src/actions/logout.ts
@@ -1,9 +1,9 @@
 'use server';
 import { removeServerCookie } from '@/utils/cookies.helper';
 
-export async function removeAuthCookie() {
+export async function removeAuthCookie(name: string) {
   // Call api to logout from server session.
 
   // Remove cookies
-  await removeServerCookie('ttn_token');
+  await removeServerCookie(name);
 }
